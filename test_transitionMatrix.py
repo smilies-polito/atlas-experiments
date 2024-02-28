b2062e798b1e9652d431c43cbffb37fe7006003f
Alessia Leclercq <s291871@studenti.polito.it>
2024-02-28 16:11:29 +0000

    Update test_transitionMatrix.py: all classes tested

--- test_transitionMatrix.py
+++ test_transitionMatrix.py
@@ -8,11 +8,13 @@ from scipy.sparse import csr_matrix
 from transitionMatrix import Similarity, SimilarityComputer, SimilarityWrapper, Cosine, Correlation, DotProduct
 from transitionMatrix import Deterministic, TransitionMatrix
 
+np.random.seed(52)
+
 class Test(unittest.TestCase):
     obs_names = ['cell1', 'cell2', 'cell3', 'cell4', 'cell5']
     var_names = ['peak1', 'peak2', 'peak3', 'peak4']
-    gene_names = ['gene0', 'gene1','gene2', 'gene3', 'gene4', 'gene5']
     X = np.random.uniform(size=(5,4))
+    gene_names = ['gene0', 'gene1','gene2', 'gene3', 'gene4', 'gene5']
     df = pd.DataFrame({'gene': ['gene5', 'gene2', 'gene0', 'gene1']}, index = var_names)
     velocities = pd.DataFrame(np.random.uniform(size=(5,6)), index = obs_names, columns = gene_names)
     connectivities = csr_matrix(np.array([[0,1,1,0,0], [1,0,1,1,0], [1,1,0,0,0], [0,1,0,0,1], [0,0,0,1,0]]))
@@ -218,6 +220,105 @@ class Test(unittest.TestCase):
         self.assertEqual(probs.toarray()[0][1].all(), p[0].all()) 
 
 
+    def test_transitionMatrix_init_error(self):
+        velo1 = np.zeros((5,5))
+        with self.assertRaises(ValueError) as ve:
+            tm = TransitionMatrix(self.adata, velo1)
+        self.assertEqual(str(ve.exception),"Shapes of velocity array (5, 5) and adata (5, 4) do not match.")
+
+        velo2 = np.zeros((6,4))
+        with self.assertRaises(ValueError) as ve:
+            tm = TransitionMatrix(self.adata, velo2)
+        self.assertEqual(str(ve.exception),"Shapes of velocity array (6, 4) and adata (5, 4) do not match.")
+
+        with self.assertRaises(ValueError) as ve:
+            TransitionMatrix(self.adata, self.velocities)
+        self.assertEqual(str(ve.exception), "With pd.DataFrame promoter_key field is mandatory" )
+
+        velocity_sized = self.velocities[1:]
+        with self.assertRaises(ValueError) as ve:
+            TransitionMatrix(self.adata, velocity_sized, promoter_key='gene')
+        self.assertEqual(str(ve.exception), "Barcordes no not match." )
+
+        new_barcodes = np.array(['cell10', 'cell2', 'cell3', 'cell4', 'cell5'])
+        velocity_wrong = self.velocities.copy().set_index(new_barcodes)
+        with self.assertRaises(ValueError) as ve:
+            TransitionMatrix(self.adata, velocity_wrong, promoter_key='gene')
+        self.assertEqual(str(ve.exception), "Barcordes no not match." )    
+
+        velocity_genes = self.velocities[self.gene_names[1:]]
+        with self.assertRaises(ValueError) as ve:
+            TransitionMatrix(self.adata, velocity_genes, promoter_key='gene')
+        self.assertEqual(str(ve.exception), "Not all genes in adata.var[gene] are in velocities")    
+
+        with self.assertRaises(ValueError) as ve:
+            TransitionMatrix(self.adata, self.velocities, promoter_key='ciao')
+        self.assertEqual(str(ve.exception), "ciao not in adata.var")    
+
+
     
+    def test_transitionMatrix_init_numpy(self):
+        velo1 = np.zeros((5,4))
+        velo1[0][0] = np.nan 
+        tm = TransitionMatrix(self.adata, velocities=velo1)
+        self.assertIsNotNone(tm._velocities)
+        self.assertIsInstance(tm._velocities, np.ndarray)
+        self.assertEqual(tm._velocities[0][0], 0.) #nans have been replaced
+
+
+    def test_transitionMatrix_init_df(self):
+        nan_velocities = self.velocities.copy()
+        nan_velocities['gene5'] = np.ones(nan_velocities.shape[0]) * np.nan
+        tm = TransitionMatrix(self.adata, nan_velocities, promoter_key='gene', softmax_scale=0.5)
+        self.assertIsNotNone(tm._velocities)
+        self.assertIsInstance(tm._velocities, np.ndarray)
+        self.assertEqual(tm._velocities[0][0], 0.) #nans have been replaced
+        self.assertEqual(tm._velocities.shape, (5,4))
+        self.assertIsNotNone(tm._adata)
+        self.assertEqual(tm._adata.shape,(5,4))
+        self.assertEqual(tm._softmax_scale, 0.5)
+        self.assertEqual(tm._promoter_key, 'gene')
+        self.assertEqual(tm.key, 'gene')
+
+
+    def test_softmax_scale_computation(self):
+        tm =  TransitionMatrix(self.adata, self.velocities, promoter_key='gene')
+        self.assertIsNone(tm._softmax_scale)
+        res = tm.estimate_softmax_scale(similarity=DotProduct(), key='connectivities')
+        
+        det = Deterministic(self.adata, 
+                            self.velocities.get(self.adata.var['gene']).to_numpy(dtype=np.float64),
+                            similarity=DotProduct(),
+                            softmax_scale=1.0,
+                            key = 'connectivities')
+        _, logits = det()
+        self.assertEqual(res, 1.0/np.median(np.abs(logits.data)))
+
+
+    def test_compute_transition_matrix_str(self):
+        tm =  TransitionMatrix(self.adata, self.velocities, promoter_key='gene')
+        tm.compute_transition_matrix('dot')
+        self.assertIsNotNone(tm._softmax_scale)
+        self.assertIsNotNone(tm._logits)
+        self.assertIsNotNone(tm._transition_matrix)
+
+
+    def test_compute_transition_matrix_similarityCom(self):
+        tm =  TransitionMatrix(self.adata, self.velocities, promoter_key='gene')
+        tm.compute_transition_matrix(Cosine())
+        self.assertIsNotNone(tm._softmax_scale)
+        self.assertIsNotNone(tm._logits)
+        self.assertIsNotNone(tm._transition_matrix)
+
+
+    def test_compute_transition_matrix_similarity(self):
+        tm =  TransitionMatrix(self.adata, self.velocities, promoter_key='gene')
+        tm.compute_transition_matrix(Similarity.DOT_PRODUCT)
+        self.assertIsNotNone(tm._softmax_scale)
+        self.assertIsNotNone(tm._logits)
+        self.assertIsNotNone(tm._transition_matrix)
+
+
+
 if __name__=='__main__':
     unittest.main()
