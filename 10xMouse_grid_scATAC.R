dc5f2e3c1e546f5b2d12267342f5af48979c59b5
Alessia Leclercq <s291871@studenti.polito.it>
2024-03-02 18:49:35 +0000

    Replace 10xMouse_grid_scATAC.R: updated version of preprocessing with Signac + stores data ready to be loaded on scanpy

--- 10xMouse_grid_scATAC.R
+++ 10xMouse_grid_scATAC.R
@@ -1,12 +1,13 @@
 library(Signac)
 library(Seurat)
+library(SeuratDisk)
 library(EnsDb.Mmusculus.v79)
 library(data.table)
 library(itertools)
 library(ggplot2)
 library(patchwork)
-set.seed(52)
 
+set.seed(52)
 #if (!require("BiocManager", quietly = TRUE))
 #  install.packages("BiocManager")
 
@@ -16,7 +17,10 @@ setwd('C:/Users/aless/Desktop/scVEMO')
 PATH = file.path(getwd(), 'filtered_feature_bc_matrix')
 CELL.ANNOTATIONS = file.path(getwd(), 'cell_annotations.tsv')
 BARCODES.METRICS = file.path(getwd(), 'e18_mouse_brain_fresh_5k_per_barcode_metrics.csv')
-SAVING.FOLDER = file.path(file.path(getwd(), 'signac_data'), 'E18_embryonic_mouse_brain')
+SAVING.FOLDER = file.path(file.path(getwd(), 'signac_data'), 'E18_embryonic_mouse_brain_ATAC')
+
+saveCSV.path <- file.path(getwd(), 'save_scATAC.R')
+source(saveCSV.path)
 
 # Load data
 counts <- Read10X(PATH)
@@ -40,14 +44,6 @@ seqlevelsStyle(annotations) <- 'UCSC'
 genome(annotations) <- 'mm10'
 Annotation(adata) <- annotations
 
-# Read Multivelo Labels and subset for common barcodes + add cell labels 
-labels <- read.table(file = CELL.ANNOTATIONS, sep = '\t', header = TRUE, row.names = 1)
-cells.use <- rownames(labels)
-adata <- subset(adata, cells = cells.use)
-metadata <- adata[[]]
-metadata <- merge(metadata, labels, by='row.names')
-adata$cell.type <- metadata$celltype
-
 # QC metrics
 adata <- TSSEnrichment(adata, fast=FALSE)
 adata <- NucleosomeSignal(object = adata)
@@ -59,11 +55,6 @@ high.TSS <- quantile(adata$TSS.enrichment, c(.98))
 low.nucleo <- quantile(adata$nucleosome_signal, c(.02))
 high.nucleo <- quantile(adata$nucleosome_signal, c(.98))
 
-p1 <- VlnPlot(adata, features = "nCount_peaks") &  theme(legend.position = 'none') & geom_hline(yintercept = low.count, colour='blue')  & geom_hline(yintercept = high.count, colour='blue')
-p2 <- VlnPlot(adata, features = "TSS.enrichment") &  theme(legend.position = 'none') & geom_hline(yintercept = low.TSS, colour='blue')  & geom_hline(yintercept = high.TSS, colour='blue')
-p3 <- VlnPlot(adata, features = "nucleosome_signal") &  theme(legend.position = 'none') &  geom_hline(yintercept = low.nucleo, colour='blue')  & geom_hline(yintercept = high.nucleo, colour='blue')
-wrap_plots(p1, p2, p3, ncol = 3)
-ggsave(file.path(SAVING.FOLDER, "scATAC_QC.png"), plot=last_plot(), width = 10, height = 6)
 
 adata <- subset(
   x = adata,
@@ -75,37 +66,46 @@ adata <- subset(
     TSS.enrichment < high.TSS
 )
 
+p1 <- VlnPlot(adata, features = "nCount_peaks") &  theme(legend.position = 'none') & geom_hline(yintercept = low.count, colour='blue')  & geom_hline(yintercept = high.count, colour='blue')
+p2 <- VlnPlot(adata, features = "TSS.enrichment") &  theme(legend.position = 'none') & geom_hline(yintercept = low.TSS, colour='blue')  & geom_hline(yintercept = high.TSS, colour='blue')
+p3 <- VlnPlot(adata, features = "nucleosome_signal") &  theme(legend.position = 'none') &  geom_hline(yintercept = low.nucleo, colour='blue')  & geom_hline(yintercept = high.nucleo, colour='blue')
+wrap_plots(p1, p2, p3, ncol = 3)
+ggsave(file.path(SAVING.FOLDER, "scATAC_QC.png"), plot=last_plot(), width = 10, height = 6)
+
 adata <- RunTFIDF(adata)
+SaveSeuratDATA(adata, SAVING.FOLDER)
 
-ATAC_preprocessing <- function(adata, k.param, n.dims, copy=T){
-  object <- copy(adata) %||% copy %||% adata
-  object <- FindTopFeatures(object, min.cutoff='q0') #all Features included
-  object <- RunSVD(object) #since features=Null uses VariableFeatures
-  depth.plot <- DepthCor(object) & geom_hline(yintercept = -0.7, color="red")  & geom_hline(yintercept = 0.7, color="red")
-  depth.title <- paste("ATAC", "DepthCor", "K", k.param, "DIMS", n.dims, sep="_") 
-  ggsave(file.path(SAVING.FOLDER, paste0(depth.title, '.png')), plot=depth.plot, width = 6, height = 6)
-
-  #UMAP, Neighbors  
-  dimensions <- 2:n.dims
-  object <- RunUMAP(object, reduction='lsi', dims=dimensions, n.neighbors = k.param)
-  umap.plot <- DimPlot(object, group.by = 'cell.type')
-  umap.title <- paste("ATAC", "UMAP", "K", k.param, "DIMS", n.dims, sep="_")
-  ggsave(file.path(SAVING.FOLDER, paste0(umap.title, '.png')), plot= umap.plot, width = 6, height = 6)
+#Preprocessing 
+ATAC_preprocessing<- function(adata, k.param, n.dims, path, copy=T){
+  data <- copy(adata) %||% copy %||% adata
+  data <- FindTopFeatures(data, min.cutoff='q0') #all Features included
+  data <- RunSVD(data) #since features=Null uses VariableFeatures
   
-  object <- FindNeighbors(object, reduction='lsi', dims=dimensions, k.param= k.param)
-  object <- FindClusters(object, verbose = FALSE, algorithm = 3)
-  cluster.plot <- DimPlot(object, label = TRUE)
+  depth.plot <- DepthCor(data) & geom_hline(yintercept = -0.7, color="red")  & geom_hline(yintercept = 0.7, color="red")
+  depth.title <- paste("ATAC", "DepthCor", "K", k.param, "DIMS", n.dims, sep="_") 
+  ggsave(file.path(path, paste0(depth.title, '.png')), plot=depth.plot, width = 6, height = 6)
+  
+  dimensions <- 2:n.dims
+  data <- RunUMAP(data, reduction='lsi', dims=dimensions, n.neighbors = k.param)
+  data <- FindNeighbors(data, reduction='lsi', dims=dimensions, k.param= k.param)
+  data <- FindNeighbors(data, reduction='lsi', dims=dimensions, k.param= k.param, return.neighbor = TRUE, compute.SNN=TRUE)
+  data <- FindClusters(data, verbose = FALSE, algorithm = 3)
   cluster.title <- paste("ATAC", "CLUSTER", "K", k.param, "DIMS", n.dims, sep="_")
-  ggsave(file.path(SAVING.FOLDER, paste0(cluster.title, '.png')), plot= cluster.plot, width = 6, height = 6)
+  cluster.plot <- DimPlot(data, label = TRUE) + ggtitle(cluster.title)
+  ggsave(file.path(path, paste0(cluster.title, '.png')), plot= cluster.plot, width = 6, height = 6)
+  
+  return(data)
 
 }
 
-
-# DA RIFARE CON expand.grid E QUALCOSA TIPO MAPPLY
-n.dims <- seq(10, 50, by=10)
-n.k <- seq(10, 90, by=10)
+n.dims <- seq(10, 30, by=5)
+n.k <- seq(10, 80, by=10)
 for(dim in n.dims){
   for(k in n.k){
-    ATAC_preprocessing(adata, k, dim)
+    path = file.path(SAVING.FOLDER, paste0('scATAC_', k, 'K', dim, 'LSI', 1.0, 'res'))
+    dir.create(path)
+    data <- ATAC_preprocessing(adata, k, dim, path)  
+    SaveSeuratCSV(data, path)
   }
 }
+
