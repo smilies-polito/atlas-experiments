62cc46b41faad02ea985c4824d05e70bc059408f
Alessia Leclercq <s291871@studenti.polito.it>
2024-03-11 16:18:54 +0000

    Replace 10XMouse_scATAC_cellRank.py: changed loop over the number of states + fixed transition probability matrix title + removed res argument for scatac

--- 10XMouse_scATAC_cellRank.py
+++ 10XMouse_scATAC_cellRank.py
@@ -3,8 +3,10 @@ import argparse
 import pandas as pd
 import numpy as np
 import scanpy as sc
+import scipy as sp
 import cellrank as cr
 
+from utils import _check_conjugate
 from cellrank.kernels import PrecomputedKernel
 from transitionMatrix import TransitionMatrix
 
@@ -21,11 +23,9 @@ parser.add_argument('-lsi', type = int, help='Integer indicating the number of d
 parser.add_argument('-res_rna', type = float, help='Float indicating the number resolution for Louvain community detection algorithm for scRNA-seq data')
 args = parser.parse_args()
 
-res_atac = 1.0 #Kept constant
-
 starting_cells = pd.read_csv(os.path.join(CWD, 'starting_cells.csv'), sep=',', index_col=0, header=0).values.flatten()
 
-atac_path = os.path.join(ADATA_FOLDER, f'10xMouse_{args.k_atac}K{args.lsi}LSI{res_atac}res.h5ad')
+atac_path = os.path.join(ADATA_FOLDER, f'10xMouse_{args.k_atac}K{args.lsi}LSI.h5ad')
 atac = sc.read_h5ad(atac_path)
 
 rna_path = os.path.join(ADATA_FOLDER, f'10xMouse_{args.k_rna}K{args.pc}PC{args.res_rna}res.h5ad')
@@ -36,46 +36,51 @@ velocities = pd.DataFrame(rna.layers['velocity'], index=rna.obs_names, columns =
 
 # Compute transition matrix
 transitionMatrix = TransitionMatrix(adata=atac, velocities=velocities, promoter_key='gene')
-transitionMatrix.compute_transition_matrix(similarity='correlation', key='distances')
+transitionMatrix.compute_transition_matrix(similarity='cosine', key='distances')
 atac.obsp['matrix'] = transitionMatrix.transition_matrix
 
 # Compute random walks
 kernel = PrecomputedKernel(object = atac, obsp_key = 'matrix')
 
-title = f'Random Walk K={args.k_atac} LSI={args.lsi} res={res_atac}'
-path = os.path.join(os.getcwd(), 'figures', f'random_walk_{args.k_atac}K{args.lsi}LSI{res_atac}res.png')
+title = f'Random Walk K={args.k_atac} LSI={args.lsi}'
+path = os.path.join(os.getcwd(), 'figures', f'random_walk_{args.k_atac}K{args.lsi}.png')
 kernel.plot_random_walks(n_sims=200, seed=seed, start_ixs = starting_cells, title=title, save = path)
+
 
 # Schur decomposition
 g = cr.estimators.GPCCA(kernel)
 g.compute_schur()
-title = f'Schur decomposition K={args.k_atac} LSI={args.lsi} res={res_atac}' 
-path = f'schurDecomposition_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
+title = f'Schur decomposition K={args.k_atac} LSI={args.lsi}' 
+path = f'schurDecomposition_{args.k_atac}K{args.lsi}LSI.png'
 g.plot_spectrum(title=title, save=path, real_only=True)
+
+print(g.eigendecomposition)
 
 # Macrostates
 eigenvalues = g.eigendecomposition['D']
-for idx, eig in enumerate(eigenvalues):
-    #At least two macrostates otherwise no sense, at most 12 macrostates to limit computations (should be enough) and considering only real values
-    if(eig.imag == 0) and idx > 0 and idx < 12: 
-        ns = idx+1
-        g.compute_macrostates(n_states=ns, cluster_key='louvain')
-        tile = f'Macrostate ({ns}) Composition K={args.k_atac} LSI={args.lsi} res={res_atac}'
-        path = f'macrostateComposition{ns}_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
-        g.plot_macrostate_composition(key='louvain', show=False, save = path, title=title)
-        title = f'Coarse Transition Matrix ({ns}) K={args.k_atac} LSI={args.lsi} res={res_atac}'
-        path = f'coarseT{ns}_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
-        g.plot_coarse_T(annotate=True, save=path, title=title)
-        g.predict_initial_states()
-        g.predict_terminal_states(allow_overlap=True)
-        title = f'Initial states ({ns}) K={args.k_atac} LSI={args.lsi} res={res_atac}'
-        path = f'initial{ns}_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
-        g.plot_macrostates(which="initial", legend_loc="right", s=100, show=False, save=path, title=title)
-        path = f'terminal{ns}_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
-        title = f'Terminal states ({ns}) K={args.k_atac} LSI={args.lsi} res={res_atac}'        
-        g.plot_macrostates(which="terminal", legend_loc="right", s=100, show=False, save=path, title=title)
-        g.compute_fate_probabilities()
-        path = f'fateProb{ns}_{args.k_atac}K{args.lsi}LSI{res_atac}res.png'
-        title = f'Fate Probabilities ({ns}) K={args.k_atac} LSI={args.lsi} res={res_atac}'
-        g.plot_fate_probabilities(same_plot=True, save = path, title=title)
+idx = 1
+
+while idx < 10:
+    # Mi aspetto 1 iniziale 4 terminali (almeno 5 stati) 
+    eig = eigenvalues[idx]
+    idx = _check_conjugate(idx, eig)
+    g.compute_macrostates(n_states=idx, cluster_key='seurat_clusters')
+    title = f'Macrostate ({idx}) Composition K={args.k_atac} LSI={args.lsi}'
+    path = f'macrostateComposition{idx}_{args.k_atac}K{args.lsi}LSI.png'
+    g.plot_macrostate_composition(key='seurat_clusters', show=False, save = path, title=title)
+    title = f'Coarse Transition Matrix ({idx}) K={args.k_atac} LSI={args.lsi}'
+    path = f'coarseT{idx}_{args.k_atac}K{args.lsi}LSI.png'
+    g.plot_coarse_T(annotate=True, save=path, title=title)
+    g.predict_initial_states()
+    g.predict_terminal_states(allow_overlap=True)
+    title = f'Initial states ({idx}) K={args.k_atac} LSI={args.lsi}'
+    path = f'initial{idx}_{args.k_atac}K{args.lsi}LSI.png'
+    g.plot_macrostates(which="initial", legend_loc="right", s=100, show=False, save=path, title=title)
+    path = f'terminal{idx}_{args.k_atac}K{args.lsi}LSI.png'
+    title = f'Terminal states ({idx}) K={args.k_atac} LSI={args.lsi}'        
+    g.plot_macrostates(which="terminal", legend_loc="right", s=100, show=False, save=path, title=title)
+    g.compute_fate_probabilities(tol = 1e-10, use_petsc=True, preconditioner='ilu')
+    path = f'fateProb{idx}_{args.k_atac}K{args.lsi}LSI.png'
+    title = f'Fate Probabilities ({idx}) K={args.k_atac} LSI={args.lsi}'
+    g.plot_fate_probabilities(same_plot=True, save = path, title=title)
 
