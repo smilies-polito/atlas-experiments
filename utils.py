f85609aa25261d4bf11a9841eaf27bd8e8543a82
Alessia Leclercq <s291871@studenti.polito.it>
2024-03-11 16:26:38 +0000

    Update utils.py: added description

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 def _check_conjugate(idx, eig):
+    """
+        Function supporting the identification of real and complex eigenvalues for the selection of the macrostates
+        params:
+            idx: integer indicating the position of the eigenvalue. If eigenvalue is real then need to compute idx+1 macrostates; if eigenvalue is  complex, then returns idx+2 to get the conjugate.
+            eig: eigenvalue.
+    """
     if np.iscomplex(eig):
         print(f"Complex value needs it conjugate, returning {idx+2} states")
         return idx+2
