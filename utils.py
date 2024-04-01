473e7ed83ec79b28c2bdcfdd439e8fe3d171b363
Alessia Leclercq <s291871@studenti.polito.it>
2024-04-01 17:50:49 +0000

    Update utils.py: correct spectral gap given the number of states

--- utils.py
+++ utils.py
@@ -7,18 +7,19 @@ def _check_conjugate(idx, eig):
     else: 
         return idx+1
     
-def _compute_spectral_gap(eigenvalues: np.ndarray):
+def _compute_spectral_gap(eigenvalues: np.ndarray, ns: int):
     """
         Function that computed the spectral gap, thence the difference between lambda_n_c and lambda_n_c+1 
         params:
             - eigenvalues: array storing the eeigenvalues for GPCCA 
+            - ns: number of macrostates
         output: 
-            - spectral gap: float (or complex value) indicating the spectral gap 
+            - spectral gap: float (or complex value) indicating the spectral gap
     """
-    return eigenvalues[0] - eigenvalues[1]
+    return eigenvalues[ns] - eigenvalues[ns+1] if ns<eigenvalues.shape[0]-1 else None
 
 
-def _check_macrostate_quality(g):
+def _check_macrostate_quality(g, ns: int):
     """
         Function that computes n_c quality according to DOI 10.1007/s11634-013-0134-6:
         Criteria are: 
@@ -28,13 +29,14 @@ def _check_macrostate_quality(g):
 
         params:
             - g: an instance of cellrank GPCCA class.
+            - ns: number of macrostates
         
         output: 
             - spectral_gap: float (or complex value) indicating the spectral gap for g
             - minChi: float indicating the minChi for g 
             - crispness: float indicating the crispness for g 
     """
-    spectral_gap = _compute_spectral_gap(g.eigendecomposition['D'])
+    spectral_gap = _compute_spectral_gap(g.eigendecomposition['D'], ns)
     minChi = np.min(g.macrostates_memberships.X)
     crispness = g._gpcca.crispness_values[0]
     return spectral_gap, minChi, crispness
