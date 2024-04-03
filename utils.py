94eff80809fae6ff51c5d127bc160dd7ed63549f
Alessia Leclercq <s291871@studenti.polito.it>
2024-04-03 15:28:19 +0000

    Update utils.py: added _plot_quality_gpcca scatter plot minChi vs crispness

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
+import os
+import pandas as pd
 import numpy as np
+import cellrank as cr
+import matplotlib.pyplot as plt 
 
-def _check_conjugate(idx, eig):
+from typing import Literal, Optional, Union, Sequence 
+seed = 52
+
+def _check_conjugate(idx: int, eig: np.ndarray):
     if np.iscomplex(eig):
         print(f"Complex value needs it conjugate, returning {idx+2} states")
+    
         return idx+2
     else: 
         return idx+1
@@ -16,10 +24,10 @@ def _compute_spectral_gap(eigenvalues: np.ndarray, ns: int):
         output: 
             - spectral gap: float (or complex value) indicating the spectral gap
     """
-    return eigenvalues[ns] - eigenvalues[ns+1] if ns<eigenvalues.shape[0]-1 else None
+    return eigenvalues[ns-1] - eigenvalues[ns] if ns<eigenvalues.shape[0] else None
 
 
-def _check_macrostate_quality(g, ns: int):
+def _check_macrostate_quality(g: cr.estimators.GPCCA, ns: int):
     """
         Function that computes n_c quality according to DOI 10.1007/s11634-013-0134-6:
         Criteria are: 
@@ -41,3 +49,26 @@ def _check_macrostate_quality(g, ns: int):
     crispness = g._gpcca.crispness_values[0]
     return spectral_gap, minChi, crispness
 
+
+def _plot_quality_gpcca(df: pd.DataFrame, **kwargs):
+    """
+        Scatter plot of minChi against crispness:
+        params:
+            - df: pd.DataFrame containing minChi and crispness values.
+    """
+    fig = plt.figure()
+    plt.scatter(df['minChi'], df['crispness'])
+    title = "GPCCA macrostates quality" 
+    title = title + "_" +str(kwargs['k']) + "K" if 'k' in kwargs.keys() else title
+    title = title + " " +str(kwargs['pc']) + "PC" if 'pc' in kwargs.keys() else title
+    title = title + " " +str(kwargs['lsi']) + "LSI" if 'lsi' in kwargs.keys() else title
+    title = title + " " +str(kwargs['res']) + "res" if 'res' in kwargs.keys() else title
+
+    path = os.path.join(os.getcwd() , 'figures', title.replace(" ", "").lower() + '.png')
+
+    plt.title(title)
+    plt.xlabel("minChi")
+    plt.ylabel("crispness")
+    for i in df.index:
+        plt.annotate(i, (df['minChi'].loc[i], df['crispness'].loc[i]))
+    fig.savefig(path)
