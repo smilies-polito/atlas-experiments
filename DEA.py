afb62b112dc1a3153eebada50efdc59bc3ee9a8d
leclercq <s291871@studenti.polito.it>
2024-06-18 10:58:41 +0000

    modified DEA.py: figures in thesis

--- DEA.py
+++ DEA.py
@@ -4,51 +4,52 @@ import pandas as pd
 import scanpy as sc
 import matplotlib.pyplot as plt
 
-def FindExpression(adata, gene):
+seed = 52
+
+LOOM_FILE = os.path.join(os.getcwd(), "data_folder", "10X_multiome_mouse_brain.loom")
+CELL_ANNOTATION_PATH = os.path.join(os.getcwd(), "cell_annotations.tsv")
+RESULTS_PATH = os.path.join(os.getcwd(), "figures")
+
+celltypes = pd.read_csv(CELL_ANNOTATION_PATH, sep='\t', header=0, index_col=0)
+celltypes_key = celltypes.columns[0]
+
+adata = sc.read_loom(LOOM_FILE)
+adata.var_names_make_unique()
+adata.obs_names = [name.split(":")[1][:-1] + "-1" for name in adata.obs_names]
+
+adata =  adata[adata.obs_names.isin(celltypes.index.values)]
+adata.obs = pd.merge(adata.obs, celltypes, how="left", left_index=True, right_index=True)
+
+sc.pp.normalize_total(adata)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+sc.tl.pca(adata, n_comps=10)
+sc.pp.neighbors(adata, n_neighbors=30, n_pcs=10)
+sc.tl.umap(adata)
+
+fig, ax = plt.subplots(figsize=(12,10))
+sc.pl.umap(adata, color=[celltypes_key], show=False, title="Cell types annotations", ax=ax, legend_loc="on data", legend_fontsize="medium")
+fig.savefig(os.path.join(RESULTS_PATH, "clusters.png"))
+
+
+def FindExpression(adata, gene, saving_path, celltype_key):
     """
     Function: given a gene plot its expression along with the clusters and saves the figure
     """
     if gene in adata.var_names:
         title = f'Expression of {gene}'
-        path = os.path.join(CWD, 'figures', f'{gene}_expression.png')
+        path = os.path.join(saving_path, f'{gene}_expression.png')
         fig, ax = plt.subplots(figsize=(8, 6))
-        sc.pl.umap(adata, color='louvain', legend_loc='on data', show=False, ax=ax)
+        sc.pl.umap(adata, color=[celltype_key], legend_loc='on data', legend_fontsize="x-small", show=False, ax=ax)
         sc.pl.umap(adata, color=gene, cmap='Reds', use_raw=False, title=title, show=False, ax=ax)
         fig.savefig(path)
 
-CWD = os.getcwd()
-seed= 52
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-k', type=int, help='Integer describing the size of the local neighborhood')
-parser.add_argument('-pc', type=int, help='Integer describing the number of principal components')
-args= parser.parse_args()
-
-ADATA_PATH = os.path.join(CWD, 'signac_folder')
-DATA_PATH= os.path.join(ADATA_PATH, f"scRNA_{args.k}K{args.pc}PC.h5ad")
-adata = sc.read_h5ad(DATA_PATH)
-
-title = f'Marker genes for every cluster (Wilcoxon) K={args.k}, PC={args.pc}'
-path = f'_{args.k}K{args.pc}PC.png'
-sc.tl.rank_genes_groups(adata, 'louvain', method='wilcoxon')
-sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False, title=title, save=path, show=False)
-
-#Dataset with 10 most espressed genes in the clusters
-result = adata.uns['rank_genes_groups']
-groups = result['names'].dtype.names
-df = pd.DataFrame(
-    {group + '_' + key[:1]: result[key][group]
-    for group in groups for key in ['names', 'pvals']}).head(10)
-
-path = os.path.join(ADATA_PATH, f'geneExpression_{args.k}K{args.pc}PC.csv')
-df.to_csv(path, sep=',', header=True)
-
 # Genes from litterature
 markers = {
-    # 'cajal-retzius' : ['Reln', 'Nrxn1', 'Trp73', 'Lhx5', 'Nhlh2'],
+    'cajal-retzius' : ['Reln', 'Nrxn1', 'Trp73', 'Lhx5', 'Nhlh2'],
     'layer2to4' : ['Satb2', 'Nrgn', 'Inhba',  'Cux2', 'Prox1'],
-    # 'forebrainGABA' : ['Slc32a1'],
-    # 'interneurons' : ['Dlx1', 'Dlx2', 'Gad1', 'Gad2', 'Bcl11b', 'Lhx6', 'Adarb2'],
+    'forebrainGABA' : ['Slc32a1'],
+    'interneurons' : ['Dlx1', 'Dlx2', 'Gad1', 'Gad2', 'Bcl11b', 'Lhx6', 'Adarb2'],
     'opc' : ['Olig2', 'Pdgfra', 'Sox10'],
     'astrocytes' : ['Vim', 'Slc1a3', 'Nes', 'Aldoc'],
     'layer5to6' : [ 'Bcl11b', 'Fezf2', 'Nrgn', 'Crym', 'Rorb', 'Nr4a2'], 
@@ -56,15 +57,11 @@ markers = {
     'radialGlia' : [ 'Vim',  'Nes'],
     'ependymal' : ['Ednrb', 'Sulf1'],
     'SVZ' : [ 'Sema3c', 'Eomes'],
-    # 'Microglia': ['Trem2', 'Ctss',]
+    'Microglia': ['Trem2', 'Ctss',]
 }
 
 genes = [] 
 [genes.extend(g) for g in markers.values()]
 
 for gene in genes:
-    FindExpression(adata, gene)
-
-title = f'Markers K={args.k} PC={args.pc}'
-path = f'litteratureMarkers_{args.k}K{args.pc}PC.png'
-sc.pl.dotplot(adata, markers, 'louvain', dendrogram=True, title = title, save = path, show=False) 
+    FindExpression(adata, gene, RESULTS_PATH, celltypes_key)
