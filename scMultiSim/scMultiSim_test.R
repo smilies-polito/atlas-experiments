90289b5d5246b949868d1752ee35d2931ed281d7
Alessia Leclercq <lrcq@poldo.local>
2025-02-25 10:23:17 +0100

    Added activity, modified differentiation tree

--- scMultiSim/scMultiSim_test.R
+++ scMultiSim/scMultiSim_test.R
@@ -16,35 +16,12 @@ list_modify <- function(curr_list, ...){
   curr_list
 }
 
-# Minimal input: differentiation tree as a R pyhlo object and controls cell population 
-# structure. Each node represents a cell type and connected nodes represent differentiation
-# replationship between cell types. 
-# Già previsti da scMultiSim ci sono i seguenti
-Phyla5(plotting=TRUE) # 5 stati terminali e 4 nodi interni 
-Phyla3(plotting=TRUE) # 3 stati terminali e 2 nodi interni 
-Phyla1() # 1 branch che connette 2 nodi 
-# Altrimenti bisogna creare un oggetto vero e proprio con ape::read.tree() o ape::rtree() 
 
-
-# Optional input 1/2: GRN. Dataframe con 3 colonne <target> <regulator> <effect>. <target> e <regulator>
-# sono nomi di geni, mentre <effect> numero che indica l'azione del regulator sul target. 
-# \Qui usiamo un esempio di GRN sample con 1139 geni 
 data(GRN_params_100)
 GRN_params <- GRN_params_100
-head(GRN_params)
-
-# Optional input 2/2: cell-cell interactions, soprattutto per simulare spatial data
 
 
-# ESEMPIO: simulare 1000 cells con 50 CIFs 
-# num.genes parametro che indica quanti geni considerare. Se non settato è numero di geni differenti nel GRN
-# mentre noi qui usiamo 100 geni con 10% geni in più che non sono regolati in alcun modo. 
-
-# cif.sigma controlla la variance di CIF e più piccolo è cif.sigma più la traiettoria dei dati è definita. 
-# diff.cif.fraction indica quanto diff-CIF impattano su creazione dei dati rispetto a non-diff-CIF
-
-# do.velocity simula RNA velocity data
-options <- list(rand.seed = 0, GRN = GRN_params, num.cells = 1000, num.cifs=50, cif.sigma=0.5, tree=Phyla5()
+options <- list(rand.seed = 0, GRN = GRN_params, num.cells = 1000, num.cifs=50, cif.sigma=0.5, tree=Phyla3()
                 , diff.cif.fraction = 0.8, do.velocity=TRUE)
 results <- sim_true_counts(options)
 results$pseudotime <- (results$cell_time - min(results$cell_time))/(max(results$cell_time) - min(results$cell_time))
@@ -63,4 +40,14 @@ write.table(results$unspliced_counts, file = "unspliced.tsv", sep = "\t", row.na
 metadata <- results$cell_meta
 metadata$pseudotime <- results$pseudotime
 write.table(metadata, file = "metadata.tsv", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+
+gene_activity <- function(gene.idx){
+  peaks <- results$region_to_gene[, gene.idx]
+  gene.activity <- colSums(results$atacseq_data * peaks)
+  return(gene.activity)
+}
+
+activity <- lapply(1:results$num_genes, gene_activity) %>% do.call(cbind, .) %>% as.data.frame() %>% setNames(paste0("gene_", 1:results$num_genes))
+write.table(activity, file = "activity.tsv", sep = "\t", row.names = TRUE, col.names = TRUE, quote = FALSE) 
 
---
+++ skeletalDev/create_activity.py
@@ -0,0 +1,66 @@
+import os, gc
+import muon as mu
+import scanpy as sc
+import pandas as pd
+import anndata as ad
+
+def create_feature_map(rna, strand:bool = False):
+	features = rna.var.copy()
+	columns = ["Chromosome", "Start", "End"]
+	if strand:
+		columns.append("Strand")
+	features = features[columns]
+	features = features.loc[~features.Start.isnull()]
+	features.Start = features.Start.astype(int)
+	features.End = features.End.astype(int) 
+	return features
+
+data_path = ... 
+fragment_path = ...
+
+
+rna = sc.read_h5ad(os.path.join(data_path, f"Whole_ATAC_326532_564709.h5ad"))
+atac = rna[:, rna.var.modality=="Peaks"]
+rna = rna[:, rna.var.modality=="Gene Expression"]
+gc.collect() #removing old anndata 
+
+gtf = pd.read_csv(os.path.join(os.getcwd(), "gene_annotations.tsv"), sep="\t", index_col=0)
+gtf.set_index("gene_name", inplace=True)
+gtf.rename(columns={"start":"Start", "end":"End", "seqname":"Chromosome", "strand":"Strand"}, inplace=True)
+
+rna.var = rna.var.merge(gtf, how="left", left_index=True, right_index=True)
+rna = rna[:, ~rna.var["Chromosome"].isna()] #removing non protein coding genes
+rna = rna[:, rna.var["Chromosome"]!="chrM"] #removing mithocondrial chromosome
+
+# RNA preprocessing
+sc.pp.normalize_total(rna)
+sc.pp.log1p(rna)
+sc.pp.highly_variable_genes(rna, n_top_genes=3000, subset=True)
+
+features = create_feature_map(rna, strand=True)
+anatomical_sites = rna.obs.anatomical_site.unique()
+run_ids = rna.obs.run_id.unique()
+gc.collect() 
+
+for site in anatomical_sites:
+	subRna = rna[rna.obs.anatomical_site==site]
+	subAtac = atac[atac.obs.anatomical_site==site]
+	activity = None
+	for run in run_ids:
+		if run in subAtac.obs.run_id.unique():
+			runAtac = subAtac[subAtac.obs.run_id==run].copy()
+			runAtac.obs_names = runAtac.obs_names.map(lambda name: name.split("-")[1] + "-1") # required to match fragments file names
+			mu.atac.tl.locate_fragments(runAtac, fragments=os.path.join(fragment_path, f"{run}_atac_fragments.tsv.gz"))
+			subAct = mu.atac.tl.count_fragments_features(runAtac, features= features, stranded=True)
+			subAct.obs_names = run + "-" + subAct.obs_names.map(lambda name: name.split("-")[0]) #required then to match barcodes with RNA again 
+			if activity is None:
+				activity = subAct
+			else:
+				activity = ad.concat([activity, subAct], axis="obs", index_unique=None)
+	subRna.write_h5ad(os.path.join(os.getcwd(), f"{site}_RNA.h5ad"))
+	activity.write_h5ad(os.path.join(os.getcwd(), f"{site}_ACTIVITY.h5ad"))
+	gc.collect()
+
+
+
+
