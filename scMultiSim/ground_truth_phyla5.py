d62ec9145e38dbdfe987aac7d3fb0f1417327852
Alessia Leclercq <lrcq@poldo.local>
2025-04-07 16:53:19 +0200

    added phyla5 ground truth

---
+++ scMultiSim/ground_truth_phyla5.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import muon as mu 
+
+if __name__=="__main__":
+	data_path = ... 
+
+	data = mu.read_h5mu(data_path)
+
+	# Verso stati terminali per cellule appartenenti al cluster stesso 
+	data.obs["6-1"] = (data.obs["rna:pop"]=="6_1").astype(int) 
+	data.obs["6-7-8-2"] = (data.obs["rna:pop"]=="8_2").astype(int)
+	data.obs["6-7-8-3"] = (data.obs["rna:pop"]== "8_3").astype(int) 
+	data.obs["6-7-9"] = ((data.obs["rna:pop"] == "9_4") | (data.obs["rna:pop"]=="9_5")).astype(int) 
+
+	# cellule degli stati intermedi: 6_7, 7_8, 8_9
+	# cellule di 7_8 possono andare o veso 8_2 o verso 8_3 con probabilità 0.5 
+	# cellule di 7_9 possono andare solo verso 6-7-9
+	data.obs["6-7-9"] = data.obs["6-7-9"] + (data.obs["rna:pop"]=="7_9").astype(int)
+	data.obs["6-7-8-2"] = data.obs["6-7-8-2"] + (data.obs["rna:pop"]=="7_8").astype(int) * 0.5 
+	data.obs["6-7-8-3"] = data.obs["6-7-8-3"] + (data.obs["rna:pop"] == "7_8").astype(int)* 0.5 
+	
+	# cellule di 6_7 possono diventare qualsiasi quindi 
+	data.obs["6-7-9"] = data.obs["6-7-9"] + (data.obs["rna:pop"] == "6_7").astype(int) * 1/3
+	data.obs["6-7-8-2"] = data.obs["6-7-8-2"] + (data.obs["rna:pop"] == "6_7").astype(int) * 1/3
+	data.obs["6-7-8-3"] = data.obs["6-7-8-3"] + (data.obs["rna:pop"] == "6_7").astype(int) * 1/3
+
+	mu.pl.embedding(data, basis="X_umap", color=["6-7-9", "6-7-8-2", "6-7-8-3", "6-1"])
+	data.obs[["6-7-9", "6-7-8-2", "6-7-8-3", "6-1"]].to_csv(os.path.join(os.getcwd(),"phyla5", "branch_assignment.tsv"), sep="\t", header=True, index=True)
+
