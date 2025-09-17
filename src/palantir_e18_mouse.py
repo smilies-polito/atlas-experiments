ef3a9f70da558909902c471bf6477fd91c4c8db6
leclercq <s291871@studenti.polito.it>
2025-09-17 08:55:35 +0000

    ended preprocessing e18 mouse and updated data folder

---
+++ src/palantir_e18_mouse.py
@@ -0,0 +1,66 @@
+import os
+import json
+import muon as mu
+import scanpy as sc
+import pandas as pd
+import numpy as np
+from src.palantir_wrapper import PalantirWrapper
+from src.palantir_plots import plot_similarity_matrix, plot_diffusion_space
+
+
+if __name__=="__main__":
+	seed = 42
+	np.random.seed(seed)
+
+	working_dir = "/scvemo"
+	data_path = os.path.join(working_dir, "data", "e18_mouse")
+	results_folder = os.path.join(working_dir, "output", "e18_mouse")
+	early_cell_path = os.path.join(data_path, "selected_cells_palantir.json")
+
+	with open(early_cell_path, "r") as f:
+		early_cell = json.load(f)
+		f.close()
+
+	exit()
+
+	#path declaration + additional files
+	data = mu.read_h5mu(os.path.join(data_path, "data.h5mu"))
+	data["rna"].obsm["X_umap"] = data.obsm["X_umap"]
+	data["rna"].obs = data.obs[["celltype"]]
+
+	rna_folder = os.path.join(results_folder, f"palantir_rna")
+	if not os.path.exists(rna_folder):
+		os.mkdir(rna_folder)
+
+	multiomics_folder = os.path.join(results_folder, f"palantir_multiomics")
+	if not os.path.exists(multiomics_folder):
+		os.mkdir(multiomics_folder)
+
+
+	#EXECUTE PALANTIR RNA
+	print("EXECUTING PALANTIR - RNA MODEL")
+	pw = PalantirWrapper()
+	pw.compute_kernel(data["rna"], knn_key="neighbors", distance_key = "distances")
+	pw.run_diffusion_maps(data["rna"], seed=seed)
+	pw.determine_multiscale_space(data["rna"])
+
+	try:
+		pw.run_palantir(data["rna"], early_cell=early_cell, seed = seed) 
+		plot_palantir_results(data = data, modality_key = "rna", embedding_key = "X_umap", pseudo_time_key = "palantir_pseudotime", entropy_key = "palantir_entropy", fate_prob_key = "palantir_fate_probabilities", save= True, saving_path = rna_folder)
+
+	except Exception as e:
+		print(e)
+
+	#EXECUTE PALANTIR MULTIOMICS
+	print("EXECUTING PALANTIR - MULTIOMICS MODEL")
+	pw = PalantirWrapper()
+	pw.compute_kernel(data)
+	pw.run_diffusion_maps(data, seed=seed)
+	pw.determine_multiscale_space(data)
+
+	try:
+		pw.run_palantir(data, early_cell=early_cell, seed = seed) 
+		plot_palantir_results(data = data, modality_key = None, embedding_key = "X_umap", pseudo_time_key = "palantir_pseudotime", entropy_key = "palantir_entropy", fate_prob_key = "palantir_fate_probabilities", save= True, saving_path = multiomics_folder)
+
+	except Exception as e:
+		print(e)
