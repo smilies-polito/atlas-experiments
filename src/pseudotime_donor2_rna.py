ceb7c260697b35d3c24fc3833fe8577701c128ec
leclercq <s291871@studenti.polito.it>
2025-09-24 07:36:28 +0000

    folder sistemati

--- src/pseudotime_donor2_rna.py
+++ src/pseudotime_donor2_rna.py
@@ -1,31 +1,27 @@
 import os
 import json
-import time
 import cellrank
-import argparse
 import muon as mu
 import scanpy as sc
 import pandas as pd
 import numpy as np
-from itertools import product
-from utils import aggregate_lineage_fate 
-from core.palantirModel.plots import plot_entropy
-from core.cellrankPseudotime.pseudokernel import PseudotimeKernelMuon
-from core.matrix_analysis import MatrixAnalyser
-from core.palantirModel.plots import plot_palantir_results
-from core.palantirModel.palantir_wrapper import PalantirWrapper 
-from core.metrics import compute_correlation, compute_f1
+from src.utils import aggregate_lineage_fate 
+from src.plots import plot_entropy
+from src.pseudokernel import PseudotimeKernelMuon, analysis_matrix
+from src.plots import plot_palantir_results
+from src.palantir_wrapper import PalantirWrapper 
+from src.metrics import compute_correlation, compute_f1
 
 if __name__=="__main__":
 	seed = 42	
 	np.random.seed(seed)
 
-	working_directory= ...
-	donor = ...
-	model = "multiomics"
-	fixed_terminal = ...
-	data_path = os.path.join(working_directory, "data", "lineage_tracing",  f"{donor}")	
-	results_folder = os.path.join(working_directory, "results", "lineage_tracing")
+	working_directory= "/scvemo"
+	donor = "donor2"
+	model = "rna"
+	fixed_terminal = True
+	data_path = os.path.join(working_directory, "data", "lineage_tracing",  donor)
+	results_folder = os.path.join(working_directory, "output", "lineage_tracing")
 	
 	threshold_scheme = "hard"
 	n_macrostates = range(4,20,2)
@@ -45,6 +41,7 @@ if __name__=="__main__":
 	saving_folder = os.path.join(results_folder, f"pseudoKernel_{donor}_{model}_{fixed_terminal}")
 	if not os.path.exists(saving_folder):
 		os.mkdir(saving_folder)
+	matrix_path = os.path.join(saving_folder, "matrix_analysis.json")
 
 	# read selected cells 
 	cell_path = os.path.join(data_path, f"selected_cells.json")
@@ -58,26 +55,38 @@ if __name__=="__main__":
 
 	# read dataset
 	data = mu.read_h5mu(os.path.join(data_path, "data.h5mu"))
+	data["rna"].obs = data.obs[["lineage", "STD.CellType"]]
+	data["rna"].obsm["X_umap"] = data.obsm["X_umap"]
 
 	pw = PalantirWrapper()
-	pw.compute_kernel(data)
-	pw.run_diffusion_maps(data, seed=seed)
-	pw.determine_multiscale_space(data)
+	pw.compute_kernel(data["rna"], knn_key ="neighbors", distance_key="connectivities")
+	pw.run_diffusion_maps(data["rna"], seed=seed)
+	pw.determine_multiscale_space(data["rna"])
 	
 	try:
-		pw.run_palantir(data, early_cell = palantir_initial_barcode, seed=seed)
+		pw.run_palantir(data["rna"], early_cell = palantir_initial_barcode, seed=seed)
 		data.update()
-		kernel = PseudotimeKernelMuon(data=data, modality_key=None, embedding_key="X_umap", connectivity_key = "wnn_connectivities", pseudotime_key = "palantir_pseudotime", group_key=["STD.CellType"])
+		kernel = PseudotimeKernelMuon(data=data, modality_key="rna", embedding_key="X_umap", connectivity_key = "connectivities", pseudotime_key = "palantir_pseudotime", group_key=["STD.CellType"])
 		kernel.compute_transition_matrix(threshold_scheme=threshold_scheme)			
+		if fixed_terminal:
+			matrix_analysis = analysis_matrix(kernel.kernel.transition_matrix)
+			with open(matrix_path, "w") as f:
+				json.dump(matrix_analysis, f)
+				f.close()
+
 		g = cellrank.estimators.GPCCA(kernel.kernel)
 		g.compute_schur()
 
-		for nms in n_macrostates:
-			if fixed_terminal :
+		for n_macrostates in macrostates_to_eval:
+			results_path = saving_folder if fixed_terminal else os.path.join(saving_folder, f"{n_macrostates}")
+			if not os.path.exists(results_path):
+				os.mkdir(results_path)
+
+			if fixed_terminal:
 				g.set_initial_states(cells["initial"])
 				g.set_terminal_states(cells["terminal"])
 			else:
-				g.compute_macrostates(n_states = nms, cluster_key="STD.CellType")
+				g.compute_macrostates(n_states = n_macrostates, cluster_key="STD.CellType")
 				g.predict_terminal_states()
 				g.predict_initial_states(allow_overlap=True)
 
@@ -85,7 +94,7 @@ if __name__=="__main__":
 			dataframe = pd.DataFrame(g.fate_probabilities.X, columns = g.fate_probabilities.names, index=data.obs_names)
 	
 			# if fix terminal then compute f1 score	
-			if fixed_terminal :		
+			if fixed_terminal:		
 				dataframe_filtered = dataframe[dataframe.index.isin(ground_truth.index.to_list())]
 				aggregate_lineage_fate(dataframe_filtered, cells["terminal"], use_barcodes=False)
 				keys = ["lymphoid", "megakaryocyte", "erythroid", "myeloid"]
@@ -96,29 +105,26 @@ if __name__=="__main__":
 				results["f1_cosine"] = None
 				results["f1_euclidean"] = None
 
-			dataframe["pseudotime"] = data.obs["palantir_pseudotime"]
+			dataframe["pseudotime"] = data.obs["rna:palantir_pseudotime"]
 			dataframe["celltype"] = data.obs["STD.CellType"]
 			dataframe["entropy"] = g.compute_lineage_priming(method="entropy")
 			dataframe["KL"] = g.compute_lineage_priming(method="kl_divergence")
 			dataframe = dataframe[["entropy", "pseudotime", "KL", "celltype"]]
 
 #	 		plot images
-			plot_path = f"fateProbs.png" if fixed_terminal else f"fateProbs_{nms}.png"
-			title = f"Fate Probabilities" + "" if fixed_terminal else f" {nms}"
-			g.plot_fate_probabilities(same_plot=True, save=os.path.join(saving_folder, plot_path), title=title, show=False)
+			plot_path = os.path.join(results_path, f"fateProbs.png" )
+			title = f"Fate Probabilities" + "" if fixed_terminal else f" {n_macrostates}"
+			g.plot_fate_probabilities(same_plot=True, save=plot_path, title=title, show=False)
 			
-			plot_path = os.path.join(saving_folder, f"entropy.png" if fixed_terminal else f"entropy_{nms}.png")
-			plot_entropy(dataframe["entropy"], data.obsm["X_umap"], save=True, saving_path = plot_path)
-			plot_path = os.path.join(saving_folder, f"kl.png" if fixed_terminal else f"kl_{nms}.png")
 			plot_entropy(dataframe["KL"], data.obsm["X_umap"], save=True, saving_path = plot_path)
 
 			if not fixed_terminal :
-				plot_path = f"macrostate_composition_{nms}.png" 
-				title = f"Macrostate Composition {nms}" 
-				g.plot_macrostate_composition(key="STD.CellType", show=False, save=os.path.join(saving_folder, plot_path), title=title)
-				plot_path = f"coarseT_{nms}.png" 
-				title = f"Corse Grained TM {nms}" 
-				g.plot_coarse_T(annotate=True, save = os.path.join(saving_folder, plot_path), title= title)
+				plot_path = os.path.join(results_path, f"macrostate_composition_{n_macrostates}.png")
+				title = f"Macrostate Composition {n_macrostates}" 
+				g.plot_macrostate_composition(key="STD.CellType", show=False, save=plot_path, title=title)
+				plot_path = os.path.join(results_path, f"coarseT_{n_macrostates}.png")
+				title = f"Corse Grained TM {n_macrostates}" 
+				g.plot_coarse_T(annotate=True, save = plot_path, title= title)
 				results["n_terminal_states"] = g.fate_probabilities.shape[1]
 
 			# Compute correlations and save results
@@ -135,8 +141,7 @@ if __name__=="__main__":
 			if fixed_terminal:
 				break
 			
-			
 	except Exception as e:
 		print(e)
-		pd.DataFrame([(donor, model, fixed_terminal, nms)], index=[0]).to_csv(failures_path, sep=",", header=False, index=False, mode="a")
+		pd.DataFrame([(donor, model, fixed_terminal, n_macrostates)], index=[0]).to_csv(failures_path, sep=",", header=False, index=False, mode="a")
 
